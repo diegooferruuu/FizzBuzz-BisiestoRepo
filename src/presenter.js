50b549420e7293ec97a021b2d64f72dba40aa466
diegooferruuu <diego.ferrufino@ucb.edu.bo>
2024-08-22 10:04:19 -0400

    Imprimir resultado en HTML

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
-import sumar from "./sumador";
+import generarFizzBuzz from "./fizzbuzz";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const num = document.querySelector("#numero");
+const form = document.querySelector("#fizzbuzz-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const number = Number.parseInt(num.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + generarFizzBuzz(number) + "</p>";
 });
